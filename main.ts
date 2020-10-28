21414fbe38621ba374ab190f924438574788651e
dante199 <35238395+dante199@users.noreply.github.com>
2020-10-28 07:48:13 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,98 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . # # #
+        . . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . # #
+        . . . . #
+        . . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . . . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        . . # . #
+        . # . . #
+        # # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # . #
+        . # # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        . . # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+})
